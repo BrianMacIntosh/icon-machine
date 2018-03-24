78440394c47cfb458be50993b7458ba6165526e2
Brian MacIntosh <brian@brianmacintosh.com>
2018-03-24 13:58:19 -0700

    change license to GPL

--- random.js
+++ random.js
@@ -1,13 +1,22 @@
 
-/**
- * Icon Machine - Random Potion Icon Generator
- * by Brian MacIntosh (brianamacintosh@gmail.com)
- * 
- * This source code is licensed under the CC-BY-3.0 license.
- * https://creativecommons.org/licenses/by/3.0/
- * 
- * Visit http://www.brianmacintosh.com/
- */
+/*
+Icon Machine - Random Potion Icon Generator
+
+Copyright 2018 Brian MacIntosh <brian@brianmacintosh.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 window.RandomArt =
 {
